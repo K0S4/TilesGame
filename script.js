61289b55ef5b80aa84bf73b5bbd8d982afe596b1
K0S4 <szymonkowalski142@gmail.com>
2021-11-12 13:06:44 +0100

    Added board generation and start page with working inputs

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
+const startingTile = document.getElementById(0);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const boardSizeNumber = urlParams.get("tiles") * urlParams.get("tiles");
 let highlightedTileNumber = 0;
-let timeLeft = 30;
+let timeLeft = urlParams.get("timer");
 let score = 0;
 let miss = 0;
+let rngOld;
 let buttonActiveClass = "button-active";
-const startingTile = document.getElementById(0);
+
 startingTile.classList.add(buttonActiveClass);
+
+//board generation
+for (i = 1; i < boardSizeNumber; i++) {
+  document.getElementById("board").innerHTML += ` <button
+    id="${i}"
+    class="button-empty"
+    onclick="handleTileClick(${i})"
+  ></button>`;
+  console.log(i);
+}
 
 // dark mode
 
 function toogleTheme(tileId) {
   let element = document.getElementById("body");
   element.classList.toggle("dark-theme");
-  
 }
-
 
 //logic
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * boardSizeNumber);
+}
+
+function getUniqueRandomNumber() {
+  let rng = getRandomNumber();
+  while (rng === rngOld) {
+    rng = getRandomNumber();
+    console.log("sameNumber");
+  }
+  rngOld = rng;
+  return rng;
 }
 
 function highlightTile(tileId) {
+  console.log("tileId:", tileId);
   const tileToClick = document.getElementById(tileId);
-  tileToClick.classList.add("button-active");
+  if (tileToClick) {
+    tileToClick.classList.add("button-active");
+  }
 }
 
 function clearHighlightTile(tileId) {
   const activatedTile = document.getElementById(tileId);
   activatedTile.classList.remove("button-active");
-
 }
 
 function handleTileClick(clickedTileNumber) {
   if (clickedTileNumber == highlightedTileNumber) {
     clearHighlightTile(highlightedTileNumber);
-    const randomNumber = getRandomNumber();
+    const randomNumber = getUniqueRandomNumber();
     score++;
+    console.log("random number", randomNumber);
     highlightedTileNumber = randomNumber;
     highlightTile(randomNumber);
   } else {
@@ -56,8 +82,7 @@ function endGame() {
 }
 
 // timer
-
-const timer = setInterval(function () {
+function timer() {
   if (timeLeft <= 0) {
     clearInterval(timer);
     endGame();
@@ -65,5 +90,31 @@ const timer = setInterval(function () {
     document.getElementById("time").innerHTML = timeLeft;
   }
   timeLeft--;
-}, 1000);
+}
+timer();
+setInterval(timer, 1000);
 
+function gameParameters() {
+  const numberLinkParameter = document.querySelector("#numberLinkInput").value;
+  console.log(numberLinkParameter);
+  const timerLinkParameter = document.querySelector(
+    "#timerDurationInput"
+  ).value;
+  console.log(timerLinkParameter);
+  if (numberLinkParameter && timerLinkParameter) {
+    document
+      .getElementById("startGameLink")
+      .setAttribute(
+        "href",
+        "game.html?tiles=" +
+          numberLinkParameter +
+          "&&timer=" +
+          timerLinkParameter
+      );
+  } else {
+  }
+}
+
+function updateTextInput(val) {
+  document.getElementById("rangeInputDisplay").innerHTML = val + "x" + val;
+}
